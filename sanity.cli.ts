8797eabaca77160191d50534110236ba0a36c76f
elevent organizer <eleventorganizerid@gmail.com>
2026-06-02 17:28:41 +0700

    chore: add Sanity Studio deployment appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,9 @@ import { defineCliConfig } from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '98jwav2j',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'jcrf1pupfvwvb8qf4cvuhjd7',
+  },
 })
