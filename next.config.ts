8281618b7865784349d0ff9f315021848860b77c
suraadityadewari-hub <suraadityadewari@gmail.com>
2026-06-17 22:38:25 +0700

    fix: allow Google Maps iframe in CSP frame-src

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' data:; connect-src 'self' https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://api.sanity.io https://*.sanity.io; frame-ancestors 'none'",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' data:; connect-src 'self' https://www.google-analytics.com https://*.google-analytics.com https://*.analytics.google.com https://api.sanity.io https://*.sanity.io; frame-src https://www.google.com https://maps.google.com; frame-ancestors 'none'",
           },
         ],
       },
